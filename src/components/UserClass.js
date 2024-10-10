ee30e0672aa005ad00649dd65d0865b01d612110
Hajimalung Baba <hajimalungbaba.786@gmail.com>
2024-10-10 23:15:11 +0530

    using another state variable

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -11,16 +11,15 @@ class UserClass extends React.Component{
         // this is the best place to create state variables
         this.state ={
             count : 0,
+            count2 : 5
         }   // this is reserved in react class components
     }
 
     increaseCnt(){
-        console.log("inc..");
         // increasing it simply does not work!! even if we bind it !!
         // this.state.count = this.state.count+1; 
         // never update state vars directly!!!
         this.setState({
-            ...this.state,
             count:this.state.count+1
         });
         // in class componenets we will have to use setState method to update state variables
@@ -30,11 +29,12 @@ class UserClass extends React.Component{
     // always need to use this. 
     render(){
         const {name,location} = this.props;
-        const {count, anotherCount} = this.state;
+        const {count, count2} = this.state;
         return (
         <div className="user-card">
             <h1>Count : {count}</h1>
-            <button onClick={this.increaseCnt.bind(this)}></button>
+            <button onClick={this.increaseCnt.bind(this)}> count + 1 </button>
+                <p>{count2}</p>
             <h2>Name: {name}</h2>
             <h3>Location: {location}</h3>
             <h4>hajimalungbaba.786@gmail.com</h4>
